78a8dc0d2a043d6a09e6d379121467c93e3a84a0
KaikeRochaDev <kaikerochadev@gmail.com>
2023-01-17 17:16:26 -0300

    Registration page and registration validations

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .utils import password_is_valid
+from django.shortcuts import redirect
 
-# Create your views here.
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
+        email = request.POST.get('email')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+        
+        return HttpResponse('Testando')
+
+def logar(request):
+    return HttpResponse('Você está na página de login')
