dd009a20046ec8b68ffa576ae34a4a9bcf8767d6
KaikeRochaDev <kaikerochadev@gmail.com>
2023-02-05 18:31:52 -0300

    changes in db

--- nutri_lab/settings.py
+++ nutri_lab/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'nutri_lab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_nutrilab',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
